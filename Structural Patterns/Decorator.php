c6d7aee44d6de77a5f4cfd1661edf395f1112991
Luke Farrell <lukefarrell@Redu-Dev-Macbook-Pro.local>
2018-08-21 09:35:42 +0100

    Facade

--- Structural Patterns/Decorator.php
+++ Structural Patterns/Decorator.php
@@ -1,1 +1,89 @@
 <?php
+
+/*
+ * The decorator pattern used to extend the functionality of
+ * concrete classes by adding to their standard behaviour.
+ */
+
+interface DataSource {
+    function readData();
+}
+
+
+class FileDataSource implements DataSource {
+    protected $filename;
+
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    public function readData()
+    {
+        $csvFile = file($this->filename);
+        $data = [];
+
+        foreach($csvFile as $line){
+            $data[] = $line;
+        }
+
+        return $data;
+    }
+}
+
+
+class JsonDecorator implements DataSource {
+    protected $dataSource;
+
+    public function __construct(DataSource $dataSource)
+    {
+        $this->dataSource = $dataSource;
+    }
+
+    // Returns the data in json format
+    public function readData(){
+        return json_encode($this->dataSource->readData());
+    }
+}
+
+class Base64Decorator implements DataSource {
+    protected $dataSource;
+
+    public function __construct(DataSource $dataSource)
+    {
+        $this->dataSource = $dataSource;
+    }
+
+    // Returns the data in base64 format
+    public function readData(){
+        return base64_encode(implode(".", $this->dataSource->readData()));
+    }
+}
+
+class StripCharactersDecorator implements DataSource {
+    protected $dataSource;
+
+    public function __construct(DataSource $dataSource)
+    {
+        $this->dataSource = $dataSource;
+    }
+
+    // Returns the data in base64 format
+    public function readData(){
+        return str_replace("$", "", $this->dataSource->readData());
+    }
+}
+
+$fileDecorator = new FileDataSource('data/analytics.csv');
+$jsonDecorator = new JsonDecorator($fileDecorator);
+$base64Decorator = new Base64Decorator($fileDecorator);
+
+echo 'Unfiltered JSON Data -> ' . $jsonDecorator->readData() . '<br>';
+echo 'Unfiltered Base64 Data -> ' . $base64Decorator->readData() . '<br>';
+
+$stripDecorator = new StripCharactersDecorator($fileDecorator);
+$filteredJsonDecorator = new JsonDecorator($stripDecorator);
+$filteredBase64Decorator = new Base64Decorator($stripDecorator);
+
+echo 'Filtered JSON Data -> ' . $filteredJsonDecorator->readData() . '<br>';
+echo 'Filtered Base64 Data -> ' . $filteredBase64Decorator->readData() . '<br>';
--- Structural Patterns/Facade.php
+++ Structural Patterns/Facade.php
@@ -1,1 +1,86 @@
 <?php
+
+/*
+ * The following is the Facade pattern to hide and decouple
+ * complex systems from the client code.
+ */
+
+class BankFacade {
+    protected $bankHandler;
+    protected $currencyTransfer;
+    protected $receiptHandler;
+
+    public function __construct(
+        BankHandler $bankHandler,
+        CurrencyTransfer $currencyTransfer,
+        RecieptHandler $receiptHandler
+    )
+    {
+        $this->bankHandler = $bankHandler;
+        $this->currencyTransfer = $currencyTransfer;
+        $this->receiptHandler = $receiptHandler;
+    }
+
+
+    public function pay($amount, $email){
+        if($this->bankHandler->checkBalance($amount)){
+            $customer = $this->currencyTransfer->findCustomer($email);
+
+            $this->currencyTransfer->transfer($customer);
+
+            $this->receiptHandler->emailReciept;
+
+            return $email . ' has been paid <br>';
+        }
+
+        return 'Not enough in bank to pay ' . $email . '<br>';
+    }
+}
+
+/*
+ * Complex subsystems
+ */
+
+class BankHandler {
+    private $balance = 100;
+
+    public function checkBalance($amount){
+        if($amount < $this->balance){
+            $this->balance = $this->balance - $amount;
+            return true;
+        }
+
+        return false;
+    }
+}
+
+class CurrencyTransfer {
+    public function findCustomer($email){
+        return 'Customer object';
+    }
+
+    public function transfer($customer){
+        return 'transfer done to ' . $customer;
+    }
+}
+
+class RecieptHandler {
+    public function emailReciept(){
+         return 'reciept has been emailed';
+    }
+}
+
+
+function clientCode(BankFacade $facade){
+    echo $facade->pay(20,'test@email.com');
+    echo $facade->pay(200,'test@email.com');
+}
+
+// Subsystems
+$bankHandler = new BankHandler();
+$currencyTransfer = new CurrencyTransfer();
+$receiptHandler = new RecieptHandler();
+
+// Facade
+$facade = new BankFacade($bankHandler, $currencyTransfer, $receiptHandler);
+clientCode($facade);
